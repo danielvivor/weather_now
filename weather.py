62afb36fbad449d8841c5485a11c71293b486ba7
Daniel Vivor <danielvivor@yahoo.com>
2026-03-04 23:22:31 +0100

    Build a label that displays the text "Enter city name:" in application window.

--- weather.py
+++ weather.py
@@ -23,6 +23,7 @@ from PyQt5.QtCore import (
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.city_label = QLabel("Enter city name: ", self)
 
 # Start the application
 if __name__ == "__main__":
