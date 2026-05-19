7e1e249ea429c279d1b5d7569e7976c8d1f223a4
Daniel Vivor <you@example.com>
2026-05-19 13:41:02 +0200

    Delete these files. They are not needed for a command line application.

--- web.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Weather CLI is deployed and running on Heroku. Use 'heroku run' to execute CLI commands."
